a29f99a45b5d237ee32c9c512fa50244e2134a98
oussama khelaifa <khlaifioussama27@gmail.com>
2024-11-05 21:45:58 +0100

    first_commit

---
+++ src/main/java/com/Autocode/demo/Modele/Employees.java
@@ -0,0 +1,16 @@
+package com.Autocode.demo.Modele;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employees {
+    private Long id ;
+    private String name ;
+    private Long id_task ;
+    private String task_name ;
+    private String task_
+}
