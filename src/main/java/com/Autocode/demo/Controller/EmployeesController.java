184ac42a0753fde2354cc22037245c4a868d37d8
oussama khelaifa <khlaifioussama27@gmail.com>
2024-11-07 23:03:20 +0100

    CRUD_Employees

--- src/main/java/com/Autocode/demo/Controller/EmployeesController.java
+++ src/main/java/com/Autocode/demo/Controller/EmployeesController.java
@@ -3,10 +3,9 @@ package com.Autocode.demo.Controller;
 import com.Autocode.demo.Modele.Employees;
 import com.Autocode.demo.Service.EmployeesService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
@@ -17,5 +16,16 @@ public class EmployeesController {
     public Employees create(@RequestBody Employees employees){
         return employeesService.creer(employees);
     }
-
+    @GetMapping("/read")
+    public List<Employees> read(){
+    return  employeesService.lire();
+    }
+    @PutMapping("/update/{id}")
+    public Employees update(@PathVariable Long id,@RequestBody Employees employees) {
+    return employeesService.modifier(id, employees);
+    }
+    @DeleteMapping("/delete/{id}")
+public String delete(@PathVariable Long id){
+    return employeesService.Supprimer(id);
+}
 }
